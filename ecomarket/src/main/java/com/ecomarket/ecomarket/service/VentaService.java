7a889bed365961bbb998ae0fcba85060147d6846
S1-DONATO <sim.segovia@duocuc.cl>
2025-05-23 00:37:59 -0400

    venta completada

--- ecomarket/src/main/java/com/ecomarket/ecomarket/service/VentaService.java
+++ ecomarket/src/main/java/com/ecomarket/ecomarket/service/VentaService.java
@@ -33,35 +33,4 @@ public class VentaService {
         ventaRepository.deleteById(id);
     }
 
-    public double calcularTotal(Long id){
-        Venta venta = ventaRepository.findById(id).orElse(new Venta());
-        double total = 0;
-        for(Producto i: venta.getProductos()){
-            total += i.getPrecio();
-        }
-        return total;
-    }
-
-    public String generarFactura(Long id){
-        Venta venta = ventaRepository.findById(id).orElse(new Venta());
-        double total=0;
-        System.out.println("========================================");
-        System.out.println("N° de venta: "+venta.getIdVenta());
-        System.out.println("Fecha de venta: "+venta.getFechaVenta());
-        System.out.println("========================================");
-        System.out.println("Factura a: "+venta.getNombre());
-        System.out.println("Direccion: "+venta.getDireccionEntrega());
-        System.out.println("========================================");
-        System.out.println("Productos vendidos:");
-        int i=0;
-        while (i < venta.getProductos().size()) {
-            System.out.println((1+i)+". "+venta.getProductos().get(i));
-            i=i+1;
-        }
-        System.out.println("========================================");
-        System.out.println("Total venta: $"+total);
-        System.out.println("========================================");
-        return "Factura generada.";
-    }
-
 }
